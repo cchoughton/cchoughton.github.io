5e8bab63b486636ff61b4ef18d3aa29d8e858f6c
cchoughton <charliehoughton7@gmail.com>
2019-08-28 16:39:18 -0400

    STYLE

--- calculator.js
+++ calculator.js
@@ -1,16 +1,16 @@
-document.getElementById("add_sem").onclick = function () {
+document.getElementById("add").onclick = function () {
     var div = document.createElement('div');
     div.className = 'semester';
     div.innerHTML='<form><label for="grade">Grade</label><input class="grade" type="text"><label for="credit_hours">Credit Hours</label><input class="credit_hours" type="number"></form>'
-    document.getElementById("semesters").appendChild(div);
+    document.getElementById("courses").appendChild(div);
 };
 
-document.getElementById("rm_sem").onclick = function () {
-    var sems = document.getElementById("semesters");
-    sems.removeChild(sems.lastChild);
+document.getElementById("rm").onclick = function () {
+    var courses = document.getElementById("courses");
+    courses.removeChild(courses.lastChild);
 };
 
 document.getElementById("calculate").onclick = function () {
-    var grades = document.getElementsByClass("grade");
-    var credit_hours = document.getElementsByClass("credit_hours");
+    var grades = document.getElementsByClassName("grade");
+    var credit_hours = document.getElementsByClassName("credit_hours");
 };
