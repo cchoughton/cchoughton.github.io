01912baa69ea0d0fe63ef6a8f304c662ae9eec96
cchoughton <charliehoughton7@gmail.com>
2019-08-28 15:45:52 -0400

    Add input fields

--- calculator.js
+++ calculator.js
@@ -1,9 +1,11 @@
 document.getElementById("add_sem").onclick = function () {
     var div = document.createElement('div');
-    div.className = 'semester';       
-    document.getElementsById("semesters")[0].appendChild(div);
+    div.className = 'semester';
+    div.innerHTML='<form><label for="grade">Grade</label><input id="grade" type="text"><label for="credit_hours">Credit Hours</label><input id="credit_hours" type="number"></form>'
+    document.getElementById("semesters").appendChild(div);
 };
 
 document.getElementById("rm_sem").onclick = function () {
-    document.getElementsById("semesters")[0].appendChild(div);
+    var sems = document.getElementById("semesters").removeChild();
+    sems.removeChild(sems.lastChild);
 };
