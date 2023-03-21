d1fa36c8d61b0ef3a65d7e8da9ffac803929ecd7
sleepy <charliehoughton7@gmail.com>
2023-03-21 02:50:41 +0000

    Try fixing the save the date anim

--- save-the-date/save-the-date.js
+++ save-the-date/save-the-date.js
@@ -1,10 +1,10 @@
 $(".action").click(function() {
-    $('.envelope_top').toggleClass('top_anim');
+    $('.top').toggleClass('top_anim');
   setTimeout(
   function() 
   {
-  $('.card').toggleClass('card_anim');
   $('.envelope').toggleClass('envelope_anim');
+  $('.card').toggleClass('card_anim');
   confetti();
   }, 1500);
   $('.action').off('click');
